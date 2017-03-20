18d1e285aa0dd5bb7cc692d4ed0ced15ace36d6b
AshrafCSharp <linux@almadafcs.com>
2017-03-19 22:53:00 -0400

    GlobalVars
    This is how to Declare the Global Variables

---
+++ GlobalVariable/GlobalVariable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalVariable
+{
+    class Program
+    {
+        static string myVal="I'm a GLOBAL Variable";  // how to creat a GLOBAL Varible 
+
+        static void printString()
+        {
+            Console.WriteLine($"Print Var From (printString Function) {myVal}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Print Var From (Main) {myVal}");
+            printString();    // call printString Fun to print the same Global variable 
+
+            Console.ReadKey();
+
+        }
+    }
+}

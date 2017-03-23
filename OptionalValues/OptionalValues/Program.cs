64bb9c4d6161eb0cc021bbc93e4d195d8f176c74
AshrafCSharp <linux@almadafcs.com>
2017-03-23 01:00:36 -0400

    OptionValue
    كيفية ارسال المتغيرات للداله عن طريق الخيارات
    بأعطائها قيم مسبقه و اذا استدعيتها دون ان ان ترسل لها قيم فسوف تتعامل مع
    القيم المسبقه

---
+++ OptionalValues/OptionalValues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalValues
+{
+    class Program
+    {
+        static void PrintVals(string name="Ashraf Ben",int myVal=7)
+        {
+            Console.WriteLine($"The Name is {name}  The Value is {myVal}");
+        }
+        static void Main(string[] args)
+        {
+            string name = "Walid Ben";
+            int myVal = 20;
+            PrintVals();
+            PrintVals(name);
+            PrintVals(name, myVal);
+            PrintVals(name: "Ayham", myVal: 50);
+            // PrintVals( , myVal);   statement Error
+            Console.ReadKey();
+        }
+    }
+}
